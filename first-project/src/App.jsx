32aca1264e63cbb4f8de653d89047e3f52f9ff91
emre yilmaz <emre.ylmz@protonmail.com>
2023-10-20 23:05:53 +0300

    first part

---
+++ first-project/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import {Footer, Blog,Features,Header, WhatGPT3, Possibility} from './container'
+import {CTA,Brand,Navbar} from './components'
+
+function App() {
+  
+  return (
+    <div className='App'>
+   <div className='gradient__bg'>
+    <Navbar/>
+    <Header/>
+   </div>
+   <Brand/>
+   <WhatGPT3/>
+   <Features/>
+   <Possibility/>
+   <CTA/>
+   <Blog/>
+   <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ first-project/src/container/possibility/Possibility.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './possibility.css'
+
+const Possibility = () => {
+  return (
+    <div>Posibility</div>
+  )
+}
+
+export default Possibility
